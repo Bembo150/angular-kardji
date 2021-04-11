8d5d05dc242258942c4565296b7a77d1b3426cfc
dasmak-dev <franlopezescandell@gmail.com>
2021-04-11 20:07:09 +0200

    primeros campos y enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule,Route } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { KardComponentComponent } from './kard-component/kard-component.component';
 import { NavComponentComponent } from './nav-component/nav-component.component';
 import { FootComponentComponent } from './foot-component/foot-component.component';
 import { FormsModule } from '@angular/forms';
+import { RegisterComponentComponent } from './register-component/register-component.component';
+
+const APP_ROUTES: Route[] = [{path:'inicio', component:AppComponent},
+{path:'kanjis', component:KardComponentComponent},
+{path:'register', component:RegisterComponentComponent},
+{path:'', redirectTo:'/inicio',pathMatch:'full'},
+{path:'**', redirectTo:'/inicio',pathMatch:'full'}];
 
 @NgModule({
   declarations: [
     AppComponent,
     KardComponentComponent,
     NavComponentComponent,
-    FootComponentComponent
+    FootComponentComponent,
+    RegisterComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(APP_ROUTES),
     HttpClientModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interface/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  type: string;
+  name: string;
+  registerDate: string;
+  lastLogIn: string;
+  password: string;
+  imageUrl: string;
+}
